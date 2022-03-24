5162715d410c637e42b9566f92fbca3162e31201
Anthony van Tonder <91330873+AnthonyVanTonder@users.noreply.github.com>
2022-03-24 15:36:40 +0200

    Update ping.js

--- slashCommands/Info/ping.js
+++ slashCommands/Info/ping.js
@@ -7,12 +7,22 @@ module.exports = {
     description: 'Check my ping!',
     run: async (client, interaction) => {
       let pembed = new MessageEmbed()
-		  .setColor('RANDOM')	
+		  .setThumbnail(`${client.user.displayAvatarURL()}`)
+      .setColor('RANDOM')	
 			.setTitle('Client Ping')
-			.addField('**Latency**', `\`${Date.now() - interaction.createdTimestamp}ms\``)
-			.addField('**API Latency**', `\`${Math.round(client.ws.ping)}ms\``)
-			.setTimestamp()
-			.setFooter(`Thanks For Using ${client.user.username}`, client.user.displayAvatarURL());
+			.addFields(
+            {
+                name: '**Latency**',
+                value: `${Date.now() - interaction.createdTimestamp}ms` ,
+                inline: true
+            },
+            {
+                name: '**API Latency**',
+                value: `${Math.round(client.ws.ping)}ms`,
+                inline: true
+            }
+        )
+.setTimestamp()        .setFooter(`Thanks For Using ${client.user.username}`, client.user.displayAvatarURL());
         interaction.reply({
           embeds: [pembed]
         });
