29827d7983b5af435f6a7fc0751463c4e7cec82b
Anthony van Tonder <91330873+AnthonyVanTonder@users.noreply.github.com>
2022-05-08 12:37:42 +0200

    Update about.js

--- slashCommands/Info/about.js
+++ slashCommands/Info/about.js
@@ -3,7 +3,7 @@
 const { MessageEmbed } = require('discord.js');
 
 module.exports = {
-   name: 'info-about',
+   name: 'about',
    description: 'Shows info about the bot',
    run: async(client, interaction) => {
      let server = client.guilds.cache.size;
@@ -17,13 +17,15 @@ module.exports = {
         .addFields(
             {
                 name: 'Hold giveaways quickly and easily!',
-                value: `Hello! I'm **${client.user.username}**, and I'm here to make it as easy as possible to hold giveaways on your Discord server! I was created by [Anthony van Tonder#2056](<https://anthonyvantonder.wordpress.com/>) (<@853462855784464416>). Written In [Discord.js](https://discord.js.org/) v${djs} and [Node.js](https://nodejs.org/) ${process.version}. Check out my commands by typing **/info-help**, and checkout my website at: https://thegiveawaybot.wordpress.com/.`
+                value: `Hello! I'm **${client.user.username}**, and I'm here to make it as easy as possible to hold giveaways on your Discord server! I was created by [Anthony van Tonder#2056](<https://anthonyvantonder.wordpress.com/>) (<@853462855784464416>). Written In [Discord.js](https://discord.js.org/) v${djs} and [Node.js](https://nodejs.org/) ${process.version}. Check out my commands by typing **/commands**, and checkout my website at: https://thegiveawaybot.wordpress.com/.`
             }
         )
       .addField("📊 Stats", `${server} Servers\n${shard} Shards`, true)
-      .addField("📌 Ping", `${client.ws.ping}ms`, true)
+      .addField("📌 Ping", `**Latency:** ${Date.now() - interaction.createdTimestamp}ms\n**API Latency:** ${Math.round(client.ws.ping)}ms`, true)
       .addField("🌐 Links", "[Website](https://thegiveawaybot.wordpress.com/)\n[Invite](https://thegiveawaybot.wordpress.com/invite/)\n[Support](https://thegiveawaybot.wordpress.com/discord/)", true)
         .setTimestamp()
         await interaction.reply({ embeds: [statsembed] });
     }
 }
+
+//Bot Coded by Anthony van Tonder Give Me Credits If Used Thank You
