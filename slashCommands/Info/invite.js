5dc5fbc83638ad989ddd81559b717db302ad734d
Anthony van Tonder <91330873+AnthonyVanTonder@users.noreply.github.com>
2022-05-08 12:40:37 +0200

    Update invite.js

--- slashCommands/Info/invite.js
+++ slashCommands/Info/invite.js
@@ -1,9 +1,9 @@
-//Bot Coded by Anthony van Tonder Give Me Credits If Used Thank You
+//Bot Coded by Anthony van Tonder Pleas Give Me Credits If Used Thank You
 
 const { MessageEmbed } = require('discord.js');
 
 module.exports = {
-   name: 'info-invite',
+   name: 'invite',
    description: 'Invite the bot to your server!',
    run: async(client, interaction) => {
       
@@ -13,9 +13,11 @@ module.exports = {
             .setTitle("Invite TheGiveawayBot")
             .setURL("https://thegiveawaybot.wordpress.com/")
             .setDescription(`🎉 Hello! I'm TheGiveawayBot! I help to make giveaways quick and easy!
-You can add me to your server with this link:\n\n🔗 https://thegiveawaybot.wordpress.com/invite \n\nCheck out my commands by typing \`/info-commands\``)
+You can add me to your server with this link:\n\n🔗 https://thegiveawaybot.wordpress.com/invite \n\nCheck out my commands by typing \`/commands\``)
             .setFooter(`Thanks For Using ${client.user.username}`, client.user.displayAvatarURL())
         .setTimestamp()
         await interaction.reply({ embeds: [statsembed] });
     }
 }
+
+//Bot Coded by Anthony van Tonder Pleas Give Me Credits If Used Thank You
