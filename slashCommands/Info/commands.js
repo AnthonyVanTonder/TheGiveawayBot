0da9bd51e105f36bb4bf8bfe073ae7ebf3661939
Anthony van Tonder <91330873+AnthonyVanTonder@users.noreply.github.com>
2022-05-08 12:38:08 +0200

    Update commands.js

--- slashCommands/Info/commands.js
+++ slashCommands/Info/commands.js
@@ -3,7 +3,7 @@
 const { MessageEmbed } = require('discord.js');
 
 module.exports = {
-   name: 'info-commands',
+   name: 'commands',
    description: 'View all the commands available to the bot!',
    run: async(client, interaction) => {
 
@@ -13,7 +13,7 @@ module.exports = {
             .setTitle("TheGiveawayBot Commands Menu")
             .setURL("https://thegiveawaybot.wordpress.com/")
             .setImage(`https://th.bing.com/th/id/R.8ed86edb03d994d8d72b007cd816da53?rik=KazBFcLLg53rcw&riu=http%3a%2f%2fwww.willbakeforbooks.com%2fwp-content%2fuploads%2f2015%2f04%2fgiveaway.jpg&ehk=DwX1GSlXH0WhNHRy2HtROaJGhbXDFi8C58YfJm7Twjg%3d&risl=&pid=ImgRaw&r=0`)
-            .setDescription(`\n\n**Here are the general bot commands:**\n\n\`/info-help\` - Shows you Help for TGB\n\`/info-commands\` - View all the commands available to the bot!\n\`/info-invite\` - Invite the bot to your server!\n\`/info-ping\` - Check the bot’s websocket latency!\n\`/info-stats\` - Sends bot physical statistics.\n\`/info-feedback\` - Send me feedback (can include bugs/problems)\n\`/info-about\` - Shows info about the bot.\n\n**Here are the giveaway bot commands:**\n\n\`/giveaway start\` - Start a giveaway\n\`‎/giveaway end\` - End an already running giveaway\n\`/giveaway reroll\` - Reroll a giveaway\n\`/giveaway edit\` - Edit a giveaway\n\`/giveaway delete\` - Delete a giveaway\n\`/giveaway resume\` - Resume a paused giveaway\n\`/giveaway pause\` - Pause a giveaway\n\n`)
+            .setDescription(`\n\n**Here are the Info commands:**\n\n\`/help\` - Shows you Help for TGB\n\`/commands\` - View all the commands available to the bot!\n\`/invite\` - Invite the bot to your server!\n\`/ping\` - Check the bot’s websocket latency!\n\`/stats\` - Sends bot physical statistics.\n\`/feedback\` - Send me feedback (can include bugs/problems)\n\`/about\` - Shows info about the bot.\n\`/vote\` - Vote For The Bot On Top.gg.\n\n**Here are the Giveaways commands:**\n\n\`/giveaway start\` - Start a giveaway\n\`‎/giveaway end\` - End an already running giveaway\n\`/giveaway reroll\` - Reroll a giveaway\n\`/giveaway edit\` - Edit a giveaway\n\`/giveaway delete\` - Delete a giveaway\n\`/giveaway resume\` - Resume a paused giveaway\n\`/giveaway pause\` - Pause a giveaway\n\`/giveaway-list\` - List all the active giveaways for this server.\n\n`)
             .setFooter(`Thanks For Using ${client.user.username}`, client.user.displayAvatarURL())
             .setTimestamp()
         await interaction.reply({ embeds: [statsembed] });
