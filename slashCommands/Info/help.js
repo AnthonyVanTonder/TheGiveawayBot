c60601b532f21d2f943603770f91f3120ffaec22
Anthony van Tonder <91330873+AnthonyVanTonder@users.noreply.github.com>
2022-05-18 16:47:13 +0200

    Update help.js

--- slashCommands/Info/help.js
+++ slashCommands/Info/help.js
@@ -3,8 +3,9 @@
 const { MessageEmbed } = require('discord.js');
 
 module.exports = {
-   name: 'help',
-   description: 'Shows you Help for TGB',
+   name: 'ghelp',
+   description: 'Shows you Help for TheGiveawayBot!',
+   toggleOff: false,
    run: async(client, interaction) => {
       
       let statsembed = new MessageEmbed()
@@ -12,10 +13,12 @@ module.exports = {
             .setThumbnail(`${client.user.displayAvatarURL()}`)
             .setTitle("TheGiveawayBot Help")
             .setURL("https://thegiveawaybot.wordpress.com/")
-            .setDescription(`:white_check_mark: Type \`/commands\` for a list of Commands\n\n:question: New to **TheGiveawayBot**? Check out our [TheGiveawayBot's FAQ](https://thegiveawaybot.wordpress.com/faq)\n\n:page_facing_up: Still need help? [Click Here](https://thegiveawaybot.wordpress.com/discord/) to join our [Discord Server](https://thegiveawaybot.wordpress.com/discord/)`)
+            .setDescription(`:white_check_mark: Type \`/gcommands\` for a list of Commands\n\n:question: New to **TheGiveawayBot**? Check out our [TheGiveawayBot's FAQ](https://thegiveawaybot.wordpress.com/faq)\n\n:page_facing_up: Still need help? [Click Here](https://thegiveawaybot.wordpress.com/discord/) to join our [Discord Server](https://thegiveawaybot.wordpress.com/discord/)`)
             .setFooter(`Thanks For Using ${client.user.username}`, client.user.displayAvatarURL())
         .setTimestamp()
-        await interaction.reply({ embeds: [statsembed] });
+        await interaction.reply({ embeds: [statsembed],
+        ephemeral: true  
+       });
     }
 }
 
