9e9b26c7870bad03442014423428ee1bca474cdc
Anthony van Tonder <91330873+AnthonyVanTonder@users.noreply.github.com>
2022-05-18 17:30:46 +0200

    Update invite.js

--- slashCommands/Info/invite.js
+++ slashCommands/Info/invite.js
@@ -1,23 +1,17 @@
 //Bot Coded by Anthony van Tonder Pleas Give Me Credits If Used Thank You
 
-const { MessageEmbed } = require('discord.js');
-
 module.exports = {
-   name: 'invite',
-   description: 'Invite the bot to your server!',
-   run: async(client, interaction) => {
-      
-      let statsembed = new MessageEmbed()
-         .setColor("#FF4900")
-            .setThumbnail(`${client.user.displayAvatarURL()}`)
-            .setTitle("Invite TheGiveawayBot")
-            .setURL("https://thegiveawaybot.wordpress.com/")
-            .setDescription(`🎉 Hello! I'm TheGiveawayBot! I help to make giveaways quick and easy!
-You can add me to your server with this link:\n\n🔗 https://thegiveawaybot.wordpress.com/invite \n\nCheck out my commands by typing \`/commands\``)
-            .setFooter(`Thanks For Using ${client.user.username}`, client.user.displayAvatarURL())
-        .setTimestamp()
-        await interaction.reply({ embeds: [statsembed] });
-    }
-}
+  name: "ginvite",
+  description: "Invite the bot to your server!",
+  toggleOff: false,
+  run: async (client, interaction) => {
+    interaction.reply({
+      content: `🎉 Hello! I'm TheGiveawayBot! I help to make giveaways quick and easy!
+You can add me to your server with this link:\n\n🔗 https://thegiveawaybot.wordpress.com/invite \n\nCheck out my commands by typing \`/gcommands\``,
+     ephemeral: true
+    });
+  },
+};
+
 
 //Bot Coded by Anthony van Tonder Pleas Give Me Credits If Used Thank You
