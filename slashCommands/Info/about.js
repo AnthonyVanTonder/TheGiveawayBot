b7404813a8ee4ebfd7a5af15ad988a78dca0ef36
Anthony van Tonder <91330873+AnthonyVanTonder@users.noreply.github.com>
2022-05-18 16:44:14 +0200

    Update about.js

--- slashCommands/Info/about.js
+++ slashCommands/Info/about.js
@@ -3,8 +3,9 @@
 const { MessageEmbed } = require('discord.js');
 
 module.exports = {
-   name: 'about',
+   name: 'gabout',
    description: 'Shows info about the bot',
+   toggleOff: false,
    run: async(client, interaction) => {
      let server = client.guilds.cache.size;
      let shard = client.ws.shards.size;
@@ -12,19 +13,21 @@ module.exports = {
      
      let statsembed = new MessageEmbed()
          .setColor("#FF4900")
-         .setDescription(`🎉 **All about ${client.user.username}** 🎉`)
-         .setFooter(`Thanks For Using ${client.user.username}`, client.user.displayAvatarURL())
+         .setFooter(`Requested by ${interaction.user.tag}`, interaction.user.displayAvatarURL())
         .addFields(
             {
                 name: 'Hold giveaways quickly and easily!',
-                value: `Hello! I'm **${client.user.username}**, and I'm here to make it as easy as possible to hold giveaways on your Discord server! I was created by [Anthony van Tonder#2056](<https://anthonyvantonder.wordpress.com/>) (<@853462855784464416>). Written In [Discord.js](https://discord.js.org/) v${djs} and [Node.js](https://nodejs.org/) ${process.version}. Check out my commands by typing **/commands**, and checkout my website at: https://thegiveawaybot.wordpress.com/.`
+                value: `Hello! I'm **${client.user.username}**, and I'm here to make it as easy as possible to hold giveaways on your Discord server! I was created by [Anthony van Tonder#2056](<https://anthonyvantonder.wordpress.com/>) (<@853462855784464416>). Written In [Discord.js](https://discord.js.org/) v${djs} and [Node.js](https://nodejs.org/) ${process.version}. Check out my commands by typing \`/gcommands\`, and checkout my website at: https://thegiveawaybot.wordpress.com/.`
             }
         )
       .addField("📊 Stats", `${server} Servers\n${shard} Shards`, true)
       .addField("📌 Ping", `**Latency:** ${Date.now() - interaction.createdTimestamp}ms\n**API Latency:** ${Math.round(client.ws.ping)}ms`, true)
       .addField("🌐 Links", "[Website](https://thegiveawaybot.wordpress.com/)\n[Invite](https://thegiveawaybot.wordpress.com/invite/)\n[Support](https://thegiveawaybot.wordpress.com/discord/)", true)
         .setTimestamp()
-        await interaction.reply({ embeds: [statsembed] });
+        await interaction.reply({ embeds: [statsembed], 
+        content: `🎉 All about **${client.user.username}** 🎉`, 
+        ephemeral: true
+       });
     }
 }
 
