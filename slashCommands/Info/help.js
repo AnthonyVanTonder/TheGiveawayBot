5c208788464251bcc3e25b4bad24e17ae4c1be81
Anthony van Tonder <91330873+AnthonyVanTonder@users.noreply.github.com>
2022-06-04 12:21:55 +0200

    Update help.js

--- slashCommands/Info/help.js
+++ slashCommands/Info/help.js
@@ -12,11 +12,10 @@ module.exports = {
          .setColor("#FF4900")
             .setThumbnail(`${client.user.displayAvatarURL()}`)
             .setTitle("TheGiveawayBot Help")
-            .setURL("https://thegiveawaybot.wordpress.com/")
-            .setDescription(`:white_check_mark: Type \`/gcommands\` for a list of Commands\n\n:question: New to **TheGiveawayBot**? Check out our [TheGiveawayBot's FAQ](https://thegiveawaybot.wordpress.com/faq)\n\n:page_facing_up: Still need help? [Click Here](https://thegiveawaybot.wordpress.com/discord/) to join our [Discord Server](https://thegiveawaybot.wordpress.com/discord/)`)
+            .setDescription(`:white_check_mark: Type \`/gcommands\` for a list of Commands\n\n:question: New to **TheGiveawayBot**? Check out our [TheGiveawayBot's FAQ](https://thegiveawaybot.vercel.app/faq.html)\n\n:page_facing_up: Still need help? [Click Here](https://thegiveawaybot.vercel.app/support.html) to join our [Discord Server](https://thegiveawaybot.vercel.app/support.html)`)
             .setFooter(`Thanks For Using ${client.user.username}`, client.user.displayAvatarURL())
         .setTimestamp()
-        await interaction.reply({ embeds: [statsembed],
+        interaction.reply({ embeds: [statsembed],
         ephemeral: true  
        });
     }
